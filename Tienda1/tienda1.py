469c9b1fa95d9fd2fff89fe69b8c168f581c18f4
isaacalfonsortt200 <isaacalfonsorodriguez12@gmail.com>
2025-03-18 22:43:08 -0500

    Update tienda1.py

--- Tienda1/tienda1.py
+++ Tienda1/tienda1.py
@@ -1,1 +1,33 @@
+"""
+Título de práctica: Tienda1
 
+
+Autor: Isaac A lfonso R odriguez
+<Ialfonsor@academia.usbbog.edu.co>
+Fecha: 2025-02-28
+"""
+
+
+class Producto:
+    def __init__(self, nombre, precio, cantidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def __str__(self):
+        return f"|{self.nombre.ljust(10)}|{str(self.cantidad).ljust(12)} unidades|{str(self.precio).ljust(8)} pesos|"
+
+def obtener_producto(numero):
+    nombre = input(f"Producto {numero}, ¿cuál es el nombre?\n> ")
+    precio = int(input(f"¿Cuál es el precio de '{nombre}'?\n> "))
+    cantidad = int(input(f"¿Qué cantidad hay de '{nombre}'?\n> "))
+    return Producto(nombre, precio, cantidad)
+
+def main():
+    productos = [obtener_producto(i + 1) for i in range(3)]
+    
+    print("\nResumen:")
+    print("|Producto  |Cantidad     |Precio     |")
+    print("-" * 36)
+    for producto in productos:
+        print(producto)
